1317161636c2b8a4d9ba9021c242fb73086516c4
ablades <audaris@gmail.com>
2020-02-08 16:50:53 -0800

    complete LL Stack

--- Code/stack.py
+++ Code/stack.py
@@ -21,27 +21,36 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        Running time: O(1) – Constant operation to insert at the beginning of the LL/Stack"""
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        if self.is_empty():
+            return None
+        else:
+            return self.list.head.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        Running time: O(1) – Item to remove is always the head"""
+        if self.is_empty():
+            raise ValueError('Stack is empty')
+        else:
+            node = self.list.head.data
+            self.list.delete(node)
+
+            return node
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
