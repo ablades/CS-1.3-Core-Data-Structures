f464b32f7c53916efba208089db60acfd50d29b8
ablades <audaris@gmail.com>
2020-03-04 19:07:07 -0800

    it works sort of

--- Code/wordjumble.py
+++ Code/wordjumble.py
@@ -1,9 +1,10 @@
 
-def calc_val(word):
+def calc_val(word, key_word):
     value = 0
     #calculate value for each letter in word using ascii
     for char in word:
-        value += ord(char) - 96
+        if char in key_word:
+            value += ord(char)
 
     return value
 
@@ -17,24 +18,30 @@ def build_dict(words):
 
 
 
-def read_words(key_word):
 
+def read_words(input_list):
+    #open dictonary
     with open("/usr/share/dict/words") as f:
         lines = f.read().lower()
+        word_list = lines.split('\n')
 
-    word_list = lines.split('\n')
-    words = list()
-    
-    for word in word_list:
-        if len(word) == len(key_word) and calc_val(key_word) == calc_val(word):
-            words.append(word)
+    master_dictonary = dict()
 
-    return words
+    #read in all keywords
+    for key_word in input_list:
+        words = list()
+        #
+        for word in word_list:
+            if len(word) == len(key_word) and calc_val(key_word, key_word) == calc_val(word, key_word):
+                words.append(word)
+
+        master_dictonary[key_word] = words
+
+    return master_dictonary
 
 
 if __name__ == "__main__":
-    read_words("tefon")
-    print(build_dict(['tefon']))
+    print(read_words(["tefon", "ogod","laisa", "laurr", "bureek", "prouot"]))
     # dict()
     # with open("/usr/share/dict/words") as f: 
     #     for line in f.readlines():
